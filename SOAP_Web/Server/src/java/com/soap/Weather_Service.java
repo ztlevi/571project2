575c51fe6c3fa48f5ca979cee08fd9ecda5dbb01
ztlevi <zhouting@umich.edu>
2017-04-02 23:49:07 -0400

    Add Rest Server

--- SOAP_Web/Server/src/java/com/soap/Weather_Service.java
+++ SOAP_Web/Server/src/java/com/soap/Weather_Service.java
@@ -28,78 +28,13 @@ import com.mysql.MySql;
 @WebService(serviceName = "Weather_Service")
 public class Weather_Service {
 
-    private JSONObject location;
-    private JSONObject weather;
-
     /**
      * This is a sample web service operation
      */
     @WebMethod(operationName = "setLocation")
-    public void retrieveData(@WebParam(name = "location") String location) {
+    public String retrieveData(@WebParam(name = "location") String location) {
         RetrieveWeatherData ob = new RetrieveWeatherData();
         JSONObject data = ob.retrieveData(location);
-        this.location = data.getJSONObject("location");
-        this.weather = data.getJSONObject("current");
-    }
-
-    @WebMethod(operationName = "getCountry")
-    public String getCountry() {
-        String country = location.getString("country");
-        return country;
-    }
-
-    @WebMethod(operationName = "getLocalTime")
-    public String getLocalTime() {
-        return location.getString("localtime");
-    }
-
-    @WebMethod(operationName = "getTemp_c")
-    public Double getTemp_c() {
-        return weather.getDouble("temp_c");
-    }
-
-    @WebMethod(operationName = "getTemp_f")
-    public Double getTemp_f() {
-        return weather.getDouble("temp_f");
-    }
-
-    @WebMethod(operationName = "getConditionText")
-    public String getConditionText() {
-        return weather.getJSONObject("condition").getString("text");
-    }
-
-    @WebMethod(operationName = "getConditionIcon")
-    public String getConditionIcon() {
-        return weather.getJSONObject("condition").getString("icon");
-    }
-
-    @WebMethod(operationName = "getWind_mph")
-    public Double getWind_mph() {
-        return weather.getDouble("wind_mph");
-    }
-
-    @WebMethod(operationName = "getWind_degree")
-    public Double getWind_degree() {
-        return weather.getDouble("wind_degree");
-    }
-
-    @WebMethod(operationName = "getWind_dir")
-    public String getWind_dir() {
-        return weather.getString("wind_dir");
-    }
-
-    @WebMethod(operationName = "getPressure_mb")
-    public Double getPressure_mb() {
-        return weather.getDouble("pressure_mb");
-    }
-
-    @WebMethod(operationName = "getHumidity")
-    public Double getHumidity() {
-        return weather.getDouble("humidity");
-    }
-
-    @WebMethod(operationName = "getVis_miles")
-    public Double getVis_miles() {
-        return weather.getDouble("vis_miles");
+        return data.toString();
     }
 }
